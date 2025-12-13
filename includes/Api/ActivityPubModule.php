ab5c07e9fd20b9f60d4e25500edb13bda8e9db22
Luca Mauri <lucamauri@users.noreply.github.com>
2025-12-13 17:33:20 +0100

    Handlers

--- includes/Api/ActivityPubModule.php
+++ includes/Api/ActivityPubModule.php
@@ -1,101 +1,121 @@
 <?php
+
 namespace MediawikiActivityPub\Api;
 
-use MediaWiki\Rest\SimpleHandler;
-use MediaWiki\Rest\Response;
-use MediaWiki\Rest\Response;
+use MediaWiki\Config\Config;
+use MediaWiki\MediaWikiServices;
 
-class ActivityPubModule extends SimpleHandler {
-
+/**
+ * Utility class for building ActivityPub responses
+ * 
+ * This class provides static helper methods used by REST handlers
+ * to construct ActivityPub Actor, Outbox, and Followers objects.
+ */
+class ActivityPubModule {
 
     /**
-     * GET /api/rest_v1/activitypub/actor
+     * Build an ActivityPub Actor object for the wiki
+     *
+     * @return array ActivityPub Actor JSON structure
      */
-    public function getActor(): Response {
-        $config = $this->getConfig();
-        $wgServer = $config->get( 'Server' );
-        $wgSitename = $config->get( 'Sitename' );
-
-        $data = [
-    public function getActor(): Response {
-        $config = $this->getConfig();
-        $wgServer = $config->get( 'Server' );
-        $wgSitename = $config->get( 'Sitename' );
-
-        $data = [
-            '@context' => 'https://www.w3.org/ns/activitystreams',
-            'id' => $wgServer . '/api/rest_v1/activitypub/actor',
-            'id' => $wgServer . '/api/rest_v1/activitypub/actor',
-            'type' => 'Service',
-            'name' => $wgSitename,
-            'preferredUsername' => str_replace( ' ', '', $wgSitename ),
-            'inbox' => $wgServer . '/api/rest_v1/activitypub/inbox',
-            'outbox' => $wgServer . '/api/rest_v1/activitypub/outbox',
-            'followers' => $wgServer . '/api/rest_v1/activitypub/followers',
-            'inbox' => $wgServer . '/api/rest_v1/activitypub/inbox',
-            'outbox' => $wgServer . '/api/rest_v1/activitypub/outbox',
-            'followers' => $wgServer . '/api/rest_v1/activitypub/followers',
-            'summary' => 'The ' . $wgSitename . ' wiki',
+    public static function buildActorObject(): array {
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'main' );
+        
+        $mainPageUrl = self::getWikiUrl( $config );
+        $actorName = $config->get( 'ActivityPubActorName' ) ?? 'Wiki';
+        $actorIcon = $config->get( 'ActivityPubActorIcon' ) ?? '/logo.png';
+        $wikiUrl = self::getWikiUrl( $config );
+        
+        return [
+            '@context' => [
+                'https://www.w3.org/ns/activitystreams',
+                'https://w3id.org/security/v1'
+            ],
+            'type' => 'Person',
+            'id' => $wikiUrl . '/activitypub/actor',
+            'name' => $actorName,
+            'preferredUsername' => strtolower( str_replace( ' ', '', $actorName ) ),
+            'summary' => 'MediaWiki instance',
+            'url' => $mainPageUrl,
+            'icon' => [
+                'type' => 'Image',
+                'url' => $wikiUrl . $actorIcon
+            ],
+            'inbox' => $wikiUrl . '/activitypub/inbox',
+            'outbox' => $wikiUrl . '/activitypub/outbox',
+            'followers' => $wikiUrl . '/activitypub/followers',
+            'following' => $wikiUrl . '/activitypub/following',
+            'publicKey' => [
+                'id' => $wikiUrl . '/activitypub/actor#main-key',
+                'owner' => $wikiUrl . '/activitypub/actor',
+                'publicKeyPem' => self::getPublicKeyPem()
+            ]
         ];
-
-        return $this->getResponseFactory()->createJson( $data );
-
-        return $this->getResponseFactory()->createJson( $data );
     }
 
-
     /**
-     * GET /api/rest_v1/activitypub/outbox
+     * Build an ActivityPub Outbox OrderedCollection
+     *
+     * @param int $limit Number of recent activities to include
+     * @return array ActivityPub OrderedCollection JSON structure
      */
-    public function getOutbox(): Response {
-        $config = $this->getConfig();
-        $wgServer = $config->get( 'Server' );
-
-        $data = [
-    public function getOutbox(): Response {
-        $config = $this->getConfig();
-        $wgServer = $config->get( 'Server' );
-
-        $data = [
+    public static function buildOutboxObject( $limit = 20 ): array {
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'main' );
+        $wikiUrl = self::getWikiUrl( $config );
+        
+        // TODO: Fetch recent activities from activity_log table
+        $activities = [];
+        
+        return [
             '@context' => 'https://www.w3.org/ns/activitystreams',
-            'id' => $wgServer . '/api/rest_v1/activitypub/outbox',
-            'id' => $wgServer . '/api/rest_v1/activitypub/outbox',
             'type' => 'OrderedCollection',
+            'id' => $wikiUrl . '/activitypub/outbox',
             'totalItems' => 0,
-            'orderedItems' => [],
-            'totalItems' => 0,
-            'orderedItems' => [],
+            'orderedItems' => $activities
         ];
-
-        return $this->getResponseFactory()->createJson( $data );
-
-        return $this->getResponseFactory()->createJson( $data );
     }
 
+    /**
+     * Build an ActivityPub Followers Collection
+     *
+     * @return array ActivityPub Collection JSON structure
+     */
+    public static function buildFollowersObject(): array {
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'main' );
+        $wikiUrl = self::getWikiUrl( $config );
+        
+        // TODO: Fetch followers from database
+        $followers = [];
+        
+        return [
+            '@context' => 'https://www.w3.org/ns/activitystreams',
+            'type' => 'Collection',
+            'id' => $wikiUrl . '/activitypub/followers',
+            'totalItems' => 0,
+            'items' => $followers
+        ];
+    }
 
     /**
-     * GET /api/rest_v1/activitypub/followers
+     * Get the wiki base URL
+     *
+     * @param Config $config MediaWiki config
+     * @return string Base URL of the wiki
      */
-    public function getFollowers(): Response {
-        $config = $this->getConfig();
-        $wgServer = $config->get( 'Server' );
+    private static function getWikiUrl( Config $config ): string {
+        $scriptPath = $config->get( 'ScriptPath' );
+        $server = $config->get( 'Server' );
+        
+        return rtrim( $server, '/' ) . $scriptPath;
+    }
 
-        $data = [
-    public function getFollowers(): Response {
-        $config = $this->getConfig();
-        $wgServer = $config->get( 'Server' );
-
-        $data = [
-            '@context' => 'https://www.w3.org/ns/activitystreams',
-            'id' => $wgServer . '/api/rest_v1/activitypub/followers',
-            'id' => $wgServer . '/api/rest_v1/activitypub/followers',
-            'type' => 'OrderedCollection',
-            'totalItems' => 0,
-            'orderedItems' => [],
-        ];
-
-        return $this->getResponseFactory()->createJson( $data );
-
-        return $this->getResponseFactory()->createJson( $data );
+    /**
+     * Get the public key PEM for actor signing
+     *
+     * @return string Public key in PEM format, or placeholder
+     */
+    private static function getPublicKeyPem(): string {
+        // TODO: Implement proper key management
+        return '-----BEGIN PUBLIC KEY-----\n...\n-----END PUBLIC KEY-----';
     }
 }
